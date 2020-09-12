4587dca80e5138f26f42c47d4c9815fd6c81e2cf
MohaSalem <64562801+MohaSalem@users.noreply.github.com>
2020-09-13 01:29:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watchOne = new Stopwatch();
+            watchOne.StartTheTime();
+            Thread.Sleep(5000);
+            watchOne.StopTheTime();
+            Console.WriteLine("The elapsed time is {0}", watchOne.ShowDuration());
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Timer
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _isRunning;
+
+        public void StartTheTime()
+        {
+            if (_isRunning)
+            {
+                throw new InvalidOperationException("A stopwatch is already running!");
+            }
+
+            _startTime = DateTime.Now;
+            _isRunning = true;
+        }
+
+        public void StopTheTime()
+        {
+            if (!_isRunning)
+            {
+                throw new InvalidOperationException("There is no running stopwatch");
+            }
+
+            _endTime = DateTime.Now;
+            _isRunning = false;
+        }
+
+        public TimeSpan ShowDuration()
+        {
+            var timeSpan = _endTime - _startTime;
+            return timeSpan;
+        }
+    }
+}
